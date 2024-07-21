c02d9e08a68479fdd76aaa9dc1c315f09176a55b
saurab <41saurab@gmail.com>
2024-07-21 16:21:22 +0545

    commit2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import "./App.css";
-import UseReducer from "./reducer";
+// import UseReducer from "./reducer";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  console.log("Logged in user : ", user);
   return (
-    <>
-      <h1 className="heading">useReducer</h1>
-      <UseReducer />
-    </>
+    <div className="App">
+      {/* <h1 className="App-heading">useReducer</h1>
+      <UseReducer /> */}
+      <header className="App-header">
+        {isAuthenticated ? <h1>Hello {user.name}</h1> : <h1>Hello, </h1>}
+
+        {isAuthenticated ? (
+          <button onClick={(e) => logout()}>Log Out</button>
+        ) : (
+          <button
+            onClick={(e) => {
+              loginWithRedirect();
+            }}
+          >
+            Login with redirect
+          </button>
+        )}
+      </header>
+    </div>
   );
 }
 
